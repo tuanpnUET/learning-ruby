f41fb782f42a32e53bb80094fcaf9d0b8f095330
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-02-09 12:16:13 +0700

    add

---
+++ Math.rb
@@ -0,0 +1,20 @@
+puts 5 + 9
+puts 2 * 3
+puts 2 ** 3
+puts 10 % 3
+num = -20
+puts num
+puts num.abs()
+num = 20.487
+puts num
+puts num.round()
+num = 20.9
+puts num.ceil()
+puts num.floor()
+num = 8
+puts "My fav number is " +  num.to_s
+num = 20.9
+puts Math.sqrt(36.3)
+puts Math.log(10)
+num = 22.2
+puts 10 / 7.0
---
+++ workingWithString.rb
@@ -0,0 +1,12 @@
+phrase = "  Viet Nam    "
+puts "012345678910"
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+puts phrase.length()
+puts phrase.include? "Vie"
+puts phrase.include? "Nam?"
+puts phrase[7]
+puts phrase[7,10]
+puts phrase.index("N")
+puts "tuan".upcase()
